ad9185f0a18edd691d8e8f29c6afe50a47d03638
rsn93323 <rupertmarty.navarro.954@my.csun.edu>
2022-03-09 10:28:07 -0800

    pulls licensed MFA users and shows authentication Methods

---
+++ Monthly MFA.ps1
@@ -0,0 +1,10 @@
+﻿#Generate list of all licensed user's MFA status
+
+Connect-MsolService
+
+Echo "Generate list of all licensed user's MFA status"
+$folderName = (Get-Date).tostring("MM-dd-yyyy-hh-mm-ss")  
+#Connect-MsolService
+Get-MsolUser -all | Where {$_.islicensed -like "True"} | Select DisplayName,UserPrincipalName,@{Name='MFAStatus'; Expression= {If($_.StrongAuthenticationRequirements.Count -ne 0) {$_.StrongAuthenticationRequirements[0].State} Else {'Disabled'}}} | Export-Csv -NoTypeInformation C:\export\MFAStatus-$folderName.csv
+
+start C:\export
